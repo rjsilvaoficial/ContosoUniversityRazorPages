0a8f832d0657f75678dedc8fc8a09f45e11b93e3
rjsilvaoficial <rjsilvaoficial@gmail.com>
2022-03-29 10:03:51 -0300

    Implementado modelo inicial, contexto, migração, instanciação de banco, configuração de logging e telas de crud (scaffolding)

---
+++ ContosoUniversity/Startup.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+
+namespace ContosoUniversity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.AddDbContext<SchoolContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SchoolContext")));
+
+
+            //Fornecido pelo diagnostics.efcore este middleware dá meios para identificar erros em migrações
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            //As linhas abaixo referem-se a controle do padrão de paginação (1024 por default, reduzido para 100 aqui)
+            //var myMaxModelBindingCollectionSize = Convert.ToInt32(
+            //Configuration["MyMaxModelBindingCollectionSize"] ?? "100");
+
+            //services.Configure<MvcOptions>(options =>
+            //       options.MaxModelBindingCollectionSize = myMaxModelBindingCollectionSize);
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //a configuração do diagnostics.efcore
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
